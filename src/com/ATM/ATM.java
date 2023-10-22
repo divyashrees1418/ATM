1bbddaddebd7d0f161a6e98bf68f77913403fb0e
divyashrees1418 <146932598+divyashrees1418@users.noreply.github.com>
2023-10-22 11:17:42 +0530

    Update ATM.java

--- src/com/ATM/ATM.java
+++ src/com/ATM/ATM.java
@@ -1,14 +1,13 @@
 package com.ATM;
-
 import java.io.*;
-
-
 public class ATM extends OptionMenu {
     
     public static void main(String[] args) throws IOException
     {
        OptionMenu opt=new OptionMenu();
-       
+
+        //calling the grtLogin menthod which is present in OptionMenu class
+        
        opt.getLogin();
     }
     
